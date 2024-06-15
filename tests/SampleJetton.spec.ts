cd4da5a04a44ff003d124b64fe29e58737c75f09
spasunsun <peshiorov@gmail.com>
2024-06-15 13:55:55 +0500

    remove test.var

--- tests/SampleJetton.spec.ts
+++ tests/SampleJetton.spec.ts
@@ -7,8 +7,6 @@ describe('SampleJetton', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
     let sampleJetton: SandboxContract<SampleJetton>;
-    const testvar = "testvar";
-    console.log(testvar);
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
